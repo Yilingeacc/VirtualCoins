4f97672d2f252bfa988c7384017f066ece658123
Edward41110 <30587580+Edward41110@users.noreply.github.com>
2020-05-03 15:06:24 +0800

    Add files via upload

--- mainTest.java
+++ mainTest.java
@@ -5,12 +5,11 @@ public class mainTest {
     public static void main(String[] args) throws Exception {
         // register
         Chain chain = new Chain();
-        User system = new User("");
         User u1 = new User("a");
         User u2 = new User("b");
         User u3 = new User("c");
 
-        // transaction
+        // transactions
         chain.mineTransaction(u1, "miner1");
         chain.transaction2Pool(u1, u2, 5);
         chain.mineTransaction(u3, "miner2");
